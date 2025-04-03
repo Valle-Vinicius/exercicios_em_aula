6751bb5f85abdd369a0b66c5c406b8ffb5b05dd6
Valle-Vinicius <vinicius.v.rodrigues6@aluno.senai.br>
2025-04-03 07:57:23 -0300

    Update novo_arquivo.py

--- novo_arquivo.py
+++ novo_arquivo.py
@@ -1,7 +1,7 @@
 valor1 = int(input("Digite um valor: "))
 valor2 = int(input("Digite um valor: "))
 valor3 = int(input("Digite um valor: "))
-media = valor1 + valor2 + valor3
+media = valor1 + valor2 + valor3/3
 
 if media > 0 and media <= 6:
     print(f"A média dos valores é {media}, você reprovou!")
