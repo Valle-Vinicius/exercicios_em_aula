17dd4d460e141c74f6e1e198aa0799eee9d32eb5
Valle-Vinicius <vinicius.v.rodrigues6@aluno.senai.br>
2025-04-03 07:56:19 -0300

    Criação de um arquivo Python.
    Criação de Arquivo utilizado para realizar testes durante as aulas práticas do SENAI.

---
+++ novo_arquivo.py
@@ -0,0 +1,9 @@
+valor1 = int(input("Digite um valor: "))
+valor2 = int(input("Digite um valor: "))
+valor3 = int(input("Digite um valor: "))
+media = valor1 + valor2 + valor3
+
+if media > 0 and media <= 6:
+    print(f"A média dos valores é {media}, você reprovou!")
+elif media > 6:
+    print(f"A média dos valores é {media}, você passou!")
